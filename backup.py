c5b66a70cd6d4c407474dd44476394b208e8b80d
Will Boyd <whboyd@wesleyan.edu>
2013-09-17 07:02:15 -0400

    Partial first commit

---
+++ backup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+import argparse
+import hashlib
+import os
+import subprocess
+import sys
+import tempfile
+
+
+def main(argv):
+	ap = argparse.ArgumentParser()
+	ap.add_argument('source', help='Source path.')
+	ap.add_argument('destination', help='Target destination. If this is an '
+		'extant directory, a uniquely-named backup file will be placed in it; '
+		'otherwise, that filename will be used for the backup.')
+	ap.add_argument('-r', '--source-root', metavar='DIR', help='The root of '
+		'the backup. This should be a prefix to the source path. This prefix '
+		'will be removed from file paths when constructing the target file.')
+	ap.add_argument('-c', '--old-checksums', metavar='FILE', help='Checksums '
+		'to compare against. If empty, all target files will be backed up; '
+		'otherwise, all non-matching and new files will be backed up. The '
+		'format should be filename, whitespace, hexadecimal checksum (as '
+		'output by e.g. md5sum).')
+	ap.add_argument('-n', '--new-checksums', metavar='FILE', help='File to '
+		'which to write checksums. The file will be overwritten with '
+		'filename, whitespace, hexadecimal checksum (as output by e.g. '
+		'md5sum).')
+	ap.add_argument('-x', '--hash-algo', metavar='ALGORITHM', default='md5',
+		help='Checksumming algorithm to use. Available options are '
+		'platform-dependent. This option affects the interpretation of '
+		'checksums in the old-checksums and new-checksums files. Default is '
+		'%(default)s.')
+	ap.add_argument('-d', '--dry-run', action='store_true',
+		help='Print backup command instead of running it.')
+	args = ap.parse_args()
+
+	# Load extant checksums
+	old_checksums = {}
+	if args.old_checksums:
+		with open(args.old_checksums) as f:
+			for line in f:
+				fname, checksum = line.split()
+				old_checksums[fname] = checksum.decode('hex')
+
+	hash_algo = hashlib.new(args.hash_algo)
+
+	# Walk the source directory
+	args.source = os.path.abspath(args.source)
+	diffs = []
+	new_checksums = {}
+	for path, dirs, files in os.walk(args.source):
+		for fname in files:
+			fname = os.path.join(path, fname)
+			# If we have specified extant checksums or requested new ones,
+			# checksum the files and keep only differences.
+			if len(old_checksums) > 0 or args.new_checksums:
+				new_checksums[fname] = hash_file(hash_algo, fname)
+				if (fname not in old_checksums or
+					old_checksums[fname] != new_checksums[fname]):
+					diffs.append(fname)
+			else:
+				diffs.append(fname)
+
+	# Package up source files
+	if os.path.isdir(args.destination):
+		args.destination = tempfile.NamedTemporaryFile(
+			dir=args.destination, suffix='.tar.gz', delete=False
+			).name
+	command = ['tar']
+	if args.source_root:
+		command.append('-C')
+		command.append(args.source_root)
+	command.append('-czf')
+	command.append(args.destination)
+	command.extend(diffs)
+
+	if args.dry_run:
+		print ' '.join(command)
+	else:
+		subprocess.check_call(command)
+
+
+def hash_file(hasher, fname):
+	hasher = hasher.copy()
+	f = open(fname)
+	filechunk = f.read(1024)
+	while filechunk:
+		hasher.update(filechunk)
+		filechunk = f.read(1024)
+	return hasher.digest()
+
+
+if __name__=='__main__':
+	main(sys.argv[1:])
+
