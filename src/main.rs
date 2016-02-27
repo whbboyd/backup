f1a79206b988cb83205ba1bf03cef8f87dd658ee
Will Boyd <whboyd@wesleyan.edu>
2016-02-27 14:34:21 -0500

    progress

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ extern crate docopt;
 extern crate rustc_serialize;
 
 use docopt::Docopt;
+use rustc_serialize::hex::FromHex;
+use std::collections::HashMap;
+use std::env::current_dir;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::path::PathBuf;
+use std::process::exit;
 
 const USAGE: &'static str = "
 Checksum-based incremental backup utility using standard tools and formats.
@@ -21,7 +29,8 @@ Options:
   -r <dir>, --source-root <dir>
                 The root of the backup. This should be a prefix to the source
                 path. This prefix will be removed from file paths when
-                constructing the destination file.
+                constructing the destination file. Default is the current
+				working directory.
   -c <file>, --old-checksums <file>
                 Checksums to compare against. If not specified, all target
                 files will be backed up; otherwise, all non-matching and new
@@ -47,14 +56,53 @@ struct Args {
 	flag_source_root: Option<String>,
 	flag_old_checksums: Option<String>,
 	flag_new_checksums: Option<String>,
-	flag_hash_algorithm: Option<String>,
+	flag_hash_algorithm: String,
 	flag_dry_run: bool,
 }
 
 fn main() {
-	println!("Hello, world!");
-	let args: Args = Docopt::new(USAGE)
+
+	// Parse commandline arguments
+	let args : Args = Docopt::new(USAGE)
 		.and_then(|d| d.decode())
 		.unwrap_or_else(|e| e.exit());
-	println!("{:?}", args);
+
+	// Figure out source root. If not specified on the commandline, it's the
+	// current directory.
+	let source_root = args.flag_source_root
+		.and_then(|d| Some(PathBuf::from(d)))
+		.unwrap_or_else(|| current_dir()
+			.unwrap_or_else(|e| {
+				println!("Couldn't use current directory as source root: {}", e);
+				exit(2);
+			})
+		);
+	if !source_root.exists() {
+		println!("Source root path does not exist");
+		exit(3);
+	}
+
+	// Load extant checksums
+	let mut old_checksums : HashMap<String, Vec<u8>> = HashMap::new();
+	match args.flag_old_checksums {
+		Some(f) => {
+			let checksums_file = File::open(f).unwrap_or_else(|e| {
+					println!("Couldn't open checksums file: {}", e);
+					exit(4);
+				});
+			let checksums_reader = BufReader::new(&checksums_file);
+			for line in checksums_reader.lines() {
+				let foo = line.unwrap();
+				let mut bar = foo.split_whitespace();
+				old_checksums.insert(bar.next().unwrap().to_string(), bar.next().unwrap().from_hex().unwrap());
+				
+			}
+			println!("{:?}", checksums_file);
+		},
+		_ => ()
+	}
+
+
+println!("{:?}", old_checksums);
+println!("{:?}",source_root);
 }
